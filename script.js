38739795451c9885288d17d4c93e3652581cf5a5
Jack Lawrence-Jones <jackvlj@gmail.com>
2020-12-25 01:06:04 +0000

    Extract renderNames

--- script.js
+++ script.js
@@ -241,25 +241,30 @@ function generateFuckCovid() {
   }
 }
 
+function renderNames(namesList) {
+  let namesString;
+  if (!names || names.length === 0) {
+    namesString = undefined;
+  } else if (names.length === 1) {
+    namesString = names[0];
+  } else if (names.length === 2) {
+    namesString = `${names[0]} and ${names[1]}`;
+  } else {
+    const allButOneNames = names.slice(0, names.length - 1);
+    const lastName = names[names.length - 1];
+    namesString = `${allButOneNames.join(', ')} and ${lastName}`;
+  }
+
+  return namesString;
+}
+
 // Run text timer
 const urlParams = new URLSearchParams(window.location.search);
 const to = urlParams.get('to');
+
 const names = to && to.split(',');
 
-let namesString;
-if (!names || names.length === 0) {
-  namesString = undefined;
-} else if (names.length === 1) {
-  namesString = names[0];
-} else if (names.length === 2) {
-  namesString = `${names[0]} and ${names[1]}`;
-} else {
-  const allButOneNames = names.slice(0, names.length - 1);
-  const lastName = names[names.length - 1];
-  namesString = `${allButOneNames.join(', ')} and ${lastName}`;
-}
-
-const messageDiv = document.getElementById('message');
+const namesString = renderNames(names);
 
 const textLines = [
   '',
@@ -272,6 +277,8 @@ const textLines = [
   "just because it's Christmas",
   '(and at Christmas you tell the truth)',
 ];
+
+const messageDiv = document.getElementById('message');
 
 function displayText(text) {
   messageDiv.innerText = text;
